248de802eb842dd40221d29417dbdddb94d26da0
NourEldin Ahmed Samir <NourEldin20201895@fci.helwan.edu.eg>
2022-08-15 21:35:14 +0200

    Adding navigation and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CartComponent } from './pages/cart/cart.component';
+import { ConfirmationComponent } from './pages/confirmation/confirmation.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product', component: DetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'confirmation', component: ConfirmationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { FormComponent } from './components/form/form.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ConfirmationComponent } from './pages/confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DialogComponent,
+    FormComponent,
+    ProductItemComponent,
+    HomeComponent,
+    DetailsComponent,
+    CartComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
